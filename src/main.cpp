1bf236a5e620bb2ecc08c0a522d141665121e3a9
unknown <Vinicius Ferreira Citadin>
2024-06-24 10:09:31 -0300

    Ajustado o valor tratado pelos módulos hx711

---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+#include <Arduino.h>
+#include <HX711.h>
+#include <USBComposite.h>
+#include "project_name_config.h"
+
+// Criando a HID e os pedais
+USBHID HID;
+HIDJoystick simplePedal(HID);
+
+// Definindo as pinagens dos sensores
+#define DAT1 PB1
+#define CLK1 PB0
+
+#define DAT2 PB11
+#define CLK2 PB10
+
+#define DAT3 PB12
+#define CLK3 PB13
+
+// Criando os módulos HX711
+HX711 acceleratorSensor;
+HX711 brakeSensor;
+HX711 clutchSensor;
+
+void setup() {
+  Serial.begin(9600);
+  HID.begin();
+  while(!USBComposite);
+
+  // Inicializando os sensores das células de carga
+  acceleratorSensor.begin(DAT1, CLK1);
+  acceleratorSensor.set_scale(CALIB_VALUE);
+  acceleratorSensor.tare();
+
+  brakeSensor.begin(DAT2, CLK2);
+  brakeSensor.set_scale(CALIB_VALUE);
+  brakeSensor.tare();
+
+  clutchSensor.begin(DAT3, CLK3);
+  clutchSensor.set_scale(CALIB_VALUE);
+  clutchSensor.tare();
+}
+
+void loop() {
+
+// Retornando os valores obtidos nas células de carga como valores de um joystick oscilando nos eixos X e Y 
+  float acceleratorValue = acceleratorSensor.get_units();
+  float brakeValue = brakeSensor.get_units();
+  float clutchValue = clutchSensor.get_units();
+
+  int accelerator = map(acceleratorValue, 0, CALIB_MIN, 0, CALIB_MAX) - 0;
+  int brake = map(brakeValue, 0, CALIB_MIN, 0, CALIB_MAX) - 0;
+  int clutch = map(clutchValue, 0, CALIB_MIN, 0, CALIB_MAX) - 0;
+
+  simplePedal.Y(accelerator * CALIB_MULT);
+  simplePedal.X(brake * CALIB_MULT);
+  simplePedal.Xrotate(clutch * CALIB_MULT);
+
+  delay(10);
+}
+
+
---
+++ src/project_name_config.h
@@ -0,0 +1,9 @@
+#ifndef PROJECT_NAME_CONFIG_H
+#define PROJECT_NAME_CONFIG_H
+
+#define CALIB_VALUE 82400 // Define o fator de calibração da célula de carga que eu usei
+#define CALIB_MAX 255 // Define o fator de calibração máximo dos módulos HX711
+#define CALIB_MIN 100 // Define o fator de calibração máximo dos módulos HX711
+#define CALIB_MULT 10 // Define o múltiplo dos valores tratados que serão enviados para o joystick
+
+#endif 
