bc5d85fe29cae88f820c3e419c0544b495d9c153
unknown <Vinicius Ferreira Citadin>
2024-06-24 10:29:43 -0300

    Arrumado o comentário da calibração mínima

--- src/project_name_config.h
+++ src/project_name_config.h
@@ -3,7 +3,7 @@
 
 #define CALIB_VALUE 82400 // Define o fator de calibração da célula de carga que eu usei
 #define CALIB_MAX 255 // Define o fator de calibração máximo dos módulos HX711
-#define CALIB_MIN 100 // Define o fator de calibração máximo dos módulos HX711
+#define CALIB_MIN 100 // Define o fator de calibração mínmo dos módulos HX711
 #define CALIB_MULT 10 // Define o múltiplo dos valores tratados que serão enviados para o joystick
 
 #endif 
